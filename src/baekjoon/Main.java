fd0ca8ccf57c1dace9136b80ab206a8d22b460e1
ajung <cwhtha@gmail.com>
2018-01-17 20:19:07 +0900

    0117 treasure(2)

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -6,7 +6,7 @@ public class Main{
 	private static class treasure{
 		private int[] a, b;
 		private int n;
-		private int[] idx;
+		private int S;
 		public treasure(){
 			//get inputs
 			Scanner scn = new Scanner(System.in);
@@ -21,27 +21,56 @@ public class Main{
 		}
 		
 		public void sorting(){
-			//sorting A
+			//calculate S
+			this.S = 0;
+			
+			boolean[] flagA = new boolean[n];
+			boolean[] flagB = new boolean[n];
 			for(int i = 0; i < n; i++){
-				//find the value of A[i]
-				for(int j = 0; j < n; j++){
-				}
+				flagA[i] = false;
+				flagB[i] = false;
 			}
+			
+			for(int i = 0; i < n; i++){
+				//find the i-th largest number among b[]
+				int max = 0, min = 0;
+				
+				do{
+					if(flagA[min])
+						min++;
+					if(flagB[max])
+						max++;
+				}while(flagA[min] || flagB[max]);
+				
+				for(int j = 0; j < n; j++){
+					if(!flagB[j] && b[j] > b[max])
+						max = j;
+					if(!flagA[j] && a[j] < a[min])
+						min = j;
+				}
+				//b[max] is the maximum among b[]
+				//and a[min] is the minimum among a[]
+				flagA[min] = true;
+				flagB[max] = true;
+				
+//				System.out.println("max: " + max + ", min: " + min);
+				
+				S += (a[min] * b[max]);
+			}
+			
+			flagA = null;
+			flagB = null;
+			return;
 		}
 		
 		public int getS(){
-			//Calculate the value of S
-			//and this function must be called after 'sorting' function is called
-			int S = 0;
-			for(int i = 0; i < n; i++)
-				S += (a[i] * b[i]); 
 			return S;
 		}
 	}
 	public static void main(String[] args){
 		treasure solution = new treasure();
 		solution.sorting();
-		System.out.println(solution.getS());
+		System.out.print(solution.getS());
 		return;
 	}
 }
