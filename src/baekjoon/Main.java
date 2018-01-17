a00c8752b5ef9cfd25072c73d531ea4c0e93dc41
ajung <cwhtha@gmail.com>
2018-01-17 20:24:47 +0900

    0117 samsung_lab(2)

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,74 +3,76 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Main{
-	private static class treasure{
-		private int[] a, b;
-		private int n;
-		private int S;
-		public treasure(){
+	private static class lab{
+		private int m, n;
+		private int[][] labMap;
+		public lab(){
 			//get inputs
 			Scanner scn = new Scanner(System.in);
-			n = scn.nextInt();
-			a = new int[n];
-			b = new int[n];
-			for(int i = 0; i < n; i++)
-				a[i] = scn.nextInt();
-			for(int i = 0; i < n; i++)
-				b[i] = scn.nextInt();
+			this.m = scn.nextInt();
+			this.n = scn.nextInt();
+			labMap = new int[m][n];
+			for(int i = 0; i < m; i++){
+				for(int j = 0; j < n; j++)
+					labMap[i][j] = scn.nextInt();
+			}
 			scn.close();
 		}
 		
-		public void sorting(){
-			//calculate S
-			this.S = 0;
-			
-			boolean[] flagA = new boolean[n];
-			boolean[] flagB = new boolean[n];
-			for(int i = 0; i < n; i++){
-				flagA[i] = false;
-				flagB[i] = false;
+		private void virus(){
+			for(int i = 0; i < m; i++){
+				for(int j = 0; j < n; j++)
+					if(labMap[i][j] == 2)
+						spread_virus(i, j);
 			}
-			
-			for(int i = 0; i < n; i++){
-				//find the i-th largest number among b[]
-				int max = 0, min = 0;
-				
-				do{
-					if(flagA[min])
-						min++;
-					if(flagB[max])
-						max++;
-				}while(flagA[min] || flagB[max]);
-				
-				for(int j = 0; j < n; j++){
-					if(!flagB[j] && b[j] > b[max])
-						max = j;
-					if(!flagA[j] && a[j] < a[min])
-						min = j;
-				}
-				//b[max] is the maximum among b[]
-				//and a[min] is the minimum among a[]
-				flagA[min] = true;
-				flagB[max] = true;
-				
-//				System.out.println("max: " + max + ", min: " + min);
-				
-				S += (a[min] * b[max]);
-			}
-			
-			flagA = null;
-			flagB = null;
-			return;
 		}
 		
-		public int getS(){
-			return S;
+		private void spread_virus(int x, int y){
+			if(labMap[x][y] == 1)	return;
+			labMap[x][y] = 2;
+			if(x != 0 && labMap[x-1][y] == 0)
+				spread_virus(x-1, y);
+			if(y != 0 && labMap[x][y-1] == 0)
+				spread_virus(x, y-1);
+			if(x != m-1 && labMap[x+1][y] == 0)
+				spread_virus(x+1, y);
+			if(y != n-1 && labMap[x][y+1] == 0)
+				spread_virus(x, y+1);
+		}
+		
+		private int safe_score(){
+			int score = 0;
+			for(int i = 0; i < m; i++){
+				for(int j = 0; j < n; j++){
+					if(labMap[i][j] == 0)
+						score++;
+				}
+			}
+			return score;
+		}
+		
+		private void printLab(){
+			for(int i = 0; i < m; i++){
+				for(int j = 0; j < n; j++)
+					System.out.print(labMap[i][j] + " ");
+				System.out.println();
+			}
+			System.out.println();
 		}
 	}
+	
 	public static void main(String[] args){
-		treasure solution = new treasure();
-		solution.sorting();
-		System.out.print(solution.getS());
-		return;
+		lab solution = new lab();
+		
+		System.out.println("Before");
+		solution.printLab();
+		
+		solution.virus();
+		
+		System.out.println("After");
+		solution.printLab();
+		
+		System.out.print(solution.safe_score());
 	}
+	
 }
---
+++ src/baekjoon/Treasure.java
@@ -0,0 +1,76 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Treasure{
+	private static class treasure{
+		private int[] a, b;
+		private int n;
+		private int S;
+		public treasure(){
+			//get inputs
+			Scanner scn = new Scanner(System.in);
+			n = scn.nextInt();
+			a = new int[n];
+			b = new int[n];
+			for(int i = 0; i < n; i++)
+				a[i] = scn.nextInt();
+			for(int i = 0; i < n; i++)
+				b[i] = scn.nextInt();
+			scn.close();
+		}
+		
+		public void sorting(){
+			//calculate S
+			this.S = 0;
+			
+			boolean[] flagA = new boolean[n];
+			boolean[] flagB = new boolean[n];
+			for(int i = 0; i < n; i++){
+				flagA[i] = false;
+				flagB[i] = false;
+			}
+			
+			for(int i = 0; i < n; i++){
+				//find the i-th largest number among b[]
+				int max = 0, min = 0;
+				
+				do{
+					if(flagA[min])
+						min++;
+					if(flagB[max])
+						max++;
+				}while(flagA[min] || flagB[max]);
+				
+				for(int j = 0; j < n; j++){
+					if(!flagB[j] && b[j] > b[max])
+						max = j;
+					if(!flagA[j] && a[j] < a[min])
+						min = j;
+				}
+				//b[max] is the maximum among b[]
+				//and a[min] is the minimum among a[]
+				flagA[min] = true;
+				flagB[max] = true;
+				
+//				System.out.println("max: " + max + ", min: " + min);
+				
+				S += (a[min] * b[max]);
+			}
+			
+			flagA = null;
+			flagB = null;
+			return;
+		}
+		
+		public int getS(){
+			return S;
+		}
+	}
+	public static void mainTreasure(String[] args){
+		treasure solution = new treasure();
+		solution.sorting();
+		System.out.print(solution.getS());
+		return;
+	}
+}
