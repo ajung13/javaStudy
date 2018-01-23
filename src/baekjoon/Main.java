0d8ea0ce6b6cfd1251aee8efda5c8731ccaa04d9
ajung <cwhtha@gmail.com>
2018-01-23 17:01:39 +0900

    0123 samsung_path

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,7 +1,37 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Main{
+	private static class Path{
+		private int n, l;
+		private int[][] input;
+		private int path;
+		public Path(){
+			Scanner scn = new Scanner(System.in);
+			n = scn.nextInt();
+			l = scn.nextInt();
+			for(int i = 0; i < n*n; i++)
+				input[i/n][i%n] = scn.nextInt();
+			scn.close();
+			
+			path = 0;
+		}
+		public int getPath() {
+			return path;
+		}
+		
+		public void solve() {
+			// TODO Auto-generated method stub
+			for(int i = 0; i < n; i++){
+				
+			}
+		}
+		
+	}
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		Path solution = new Path();
+		solution.solve();
+		System.out.println(solution.getPath());
 	}
 }
