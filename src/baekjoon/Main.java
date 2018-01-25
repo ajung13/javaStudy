18788784f699eb0505ee4c01411dffa9a2ee9e8e
ajung <cwhtha@gmail.com>
2018-01-25 16:37:54 +0900

    0125 samsung_resign(1)

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,7 +1,35 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Main{
+	private static class Resign{
+		private int n;
+		private int[] T, P;
+		private int profit;
+		public Resign(){
+			Scanner scn = new Scanner(System.in);
+			n = scn.nextInt();
+			T = new int[n];
+			P = new int[n];
+			for(int i = 0; i < n; i++){
+				T[i] = scn.nextInt();
+				P[i] = scn.nextInt();
+			}
+			scn.close();
+			profit = 0;
+		}
+		public void solve() {
+			// TODO Auto-generated method stub
+			
+		}
+		public int getProfit() {
+			return profit;
+		}
+	}
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		Resign solution = new Resign();
+		solution.solve();
+		System.out.print(solution.getProfit());
 	}
 }
