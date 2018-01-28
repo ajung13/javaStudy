879c93fbc14197e0e9fa856b96445a61fc9e2201
ajung <cwhtha@gmail.com>
2018-01-28 20:43:34 +0900

    0128 sogang17_FA

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,7 +1,28 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Main{
 	public static void main(String[] args){
-		System.out.println("Hello world");
+		int num;
+		
+		Scanner scn = new Scanner(System.in);
+		num = scn.nextInt();
+		scn.close();
+		
+		if(check(num))
+			System.out.print("FA");
+		else
+			System.out.print("NFA");
+	}
+	
+	private static boolean check(int n){
+		int digit;
+//		while(true){
+			digit = (int)Math.log10(n)+1;
+			
+			System.out.println(digit);
+//		}
+		return false;
 	}
 }
