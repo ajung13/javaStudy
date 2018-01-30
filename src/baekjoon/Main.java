b8b71168f6cd0596d0a32896186b5141ccfd1f60
ajung <cwhtha@gmail.com>
2018-01-30 16:18:56 +0900

    0130 samsung_tetromino

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,7 +3,66 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Main{
-	public static void main(String[] args){
+	private static class Tet{
+		private int m, n;
+		private int[][] map;
+		private int max;
 		
+		public Tet(){
+			Scanner scn = new Scanner(System.in);
+			m = scn.nextInt();
+			n = scn.nextInt();
+			map = new int[m][n];
+			for(int i = 0; i < m; i++){
+				for(int j = 0; j < n; j++)
+					map[i][j] = scn.nextInt();
+			}
+			scn.close();
+			max = 0;
+		}
+		public void switchFlag(){
+			boolean[][] flag = new boolean[m][n];
+			
+			//first, find the max
+			int max = 0;
+			for(int i = 0; i < m; i++){
+				for(int j = 0; j < n; j++){
+					if(map[i][j] > max)
+						max = map[i][j];
+					flag[i][j] = false;
+				}
+			}
+			
+			//set flags
+			for(int k = max; k > 0; k--){
+				for(int i = 0; i < m; i++){
+					for(int j = 0; j < n; j++){
+						if(map[i][j] == max)
+							flag[i][j] = true;
+					}
+				}
+				//check whether there are tetrominos
+				if(findTetro(flag))
+					break;
+			}
+			
+			return;
+		}
+		private boolean findTetro(boolean[][] flag) {
+			for(int i = 0; i < m; i++){
+				for(int j = 0; j < n; j++){
+					if(flag[i][j])
+				}
+			}
+			return false;
+		}
+		public int getMax(){
+			return max;
+		}
+	}
+	public static void main(String[] args){
+		Tet solution = new Tet();
+		solution.switchFlag();
+		System.out.print(solution.getMax());
 	}
 }
