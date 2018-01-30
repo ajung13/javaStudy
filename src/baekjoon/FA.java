1f044cede0561a3884b11b9d1872d1a290022256
ajung <cwhtha@gmail.com>
2018-01-30 14:31:43 +0900

    0130 samsung_dice(1)

---
+++ src/baekjoon/FA.java
@@ -0,0 +1,29 @@
+//¹Ì¿Ï¼º!!!!!!!!!!!
+package baekjoon;
+
+import java.util.Scanner;
+
+public class FA{
+	public static void mainFA(String[] args){
+		int num;
+		
+		Scanner scn = new Scanner(System.in);
+		num = scn.nextInt();
+		scn.close();
+		
+		if(check(num))
+			System.out.print("FA");
+		else
+			System.out.print("NFA");
+	}
+	
+	private static boolean check(int n){
+		int digit;
+//		while(true){
+			digit = (int)Math.log10(n)+1;
+			
+			System.out.println(digit);
+//		}
+		return false;
+	}
+}
