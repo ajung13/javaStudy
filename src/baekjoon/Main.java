936174d3f7b8d3f994a1676b99c770b9cd28842f
ajung <cwhtha@gmail.com>
2018-02-04 14:07:14 +0900

    0204 samsung_exam(1)

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,7 +3,40 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Main{
+	private static class Sigam{
+		private int n, b, c;
+		private int[] students;
+		private long director;
+		public Sigam(){
+			Scanner scn = new Scanner(System.in);
+			n = scn.nextInt();
+			students = new int[n];
+			for(int i = 0; i < n; i++)
+				students[i] = scn.nextInt();
+			b = scn.nextInt();
+			c = scn.nextInt();
+			scn.close();
+			director = 0;
+		}
+		private void solve(){
+			//count main directors
+			for(int i = 0; i < n; i++)
+				students[i] -= b;
+			director += n;
+			
+			for(int i = 0; i < n; i++){
+				if(students[i] <= 0)
+					continue;
+				if(students[i]%c == 0)
+					director += (students[i]/c);
+				else
+					director += (students[i]/c + 1);
+			}
+		}
+	}
 	public static void main(String[] args){
-		
+		Sigam solution = new Sigam();
+		solution.solve();
+		System.out.print(solution.director);
 	}
 }
