5a59c8f905c6efcce8fb305a3025fb430f7b44df
ajung <cwhtha@gmail.com>
2018-03-11 15:58:25 +0900

    0311 make1(2)

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,33 +3,7 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Main{
-	private static long min;
 	public static void main(String[] args){
-		int n;
 		
-		Scanner scn = new Scanner(System.in);
-		n = scn.nextInt();
-		scn.close();
-		
-		min = Integer.MAX_VALUE;
-		dp(n, 0);
-		
-		System.out.print(min);
-	}
-	
-	private static void dp(long left, int cnt){
-		if(left == 1){
-			if(cnt < min)
-				min = cnt;
-			return;
-		}
-		if(left < 1)
-			return;
-		
-		if(left % 3 == 0)
-			dp(left/3, cnt+1);
-		if(left % 2 == 0)
-			dp(left/2, cnt+1);
-		dp(left-1, cnt+1);
 	}
 }
