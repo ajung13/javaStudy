be910b5a880dc4ddcfd5ea3d8b7f8904abb68cb6
ajung <cwhtha@gmail.com>
2018-03-11 16:21:03 +0900

    0311 wine(1)

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class Main{
 	public static void main(String[] args){
+		int wineNum;
+		int[] wine;
+		
+		Scanner scn = new Scanner(System.in);
+		wineNum = scn.nextInt();
+		wine = new int[wineNum];
+		for(int i = 0; i < wineNum; i++)
+			wine[i] = scn.nextInt();
+		scn.close();
+		
 		
 	}
 }
